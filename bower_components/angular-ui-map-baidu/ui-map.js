a6e74bebd3547dae608323556e578009a41072a1
anypossible.w <anypossible.w@gmail.com>
2015-02-11 22:05:52 +0800

    add cache map

--- bower_components/angular-ui-map-baidu/ui-map.js
+++ bower_components/angular-ui-map-baidu/ui-map.js
@@ -1,13 +1,48 @@
+/**!
+ * The MIT License
+ *
+ * Copyright (c) 2013 the angular-ui-map-baidu Team, http://anypossiblew.github.io/ui-map-baidu
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * angular-ui-map-baidu
+ * https://github.com/anypossiblew/ui-map-baidu
+ *
+ * @authors https://github.com/anypossiblew/ui-map-baidu/graphs/contributors
+ */
 'use strict';
 (function () {
   var app = angular.module('ui.map', ['ui.event']);
-  //Setup map events from a google map object to trigger on a given element too,
-  //then we just use ui-event to catch events from an element
-  function bindMapEvents(scope, eventsStr, mapObject, element) {
+
+  /**
+   * Setup map events from a map object to trigger on a given element too,
+   * then we just use ui-event to catch events from an element
+   *
+   * @param scope
+   * @param eventsStr
+   * @param mapObject
+   * @param element
+   * @param eventListeners
+   */
+  function bindMapEvents(scope, eventsStr, mapObject, element, eventListeners) {
     angular.forEach(eventsStr.split(' '), function (eventName) {
-      //Prefix all googlemap events with 'map-', so eg 'click'
-      //for the baidu map doesn't interfere with a normal 'click' event
-      mapObject.addEventListener(eventName, function (event) {
+      eventListeners[eventName] = function (event) {
         element.triggerHandler('map-' + eventName, event);
         //We create an $apply if it isn't happening. we need better support for this
         //We don't want to use timeout because tons of these events fire at once,
@@ -15,11 +50,29 @@
         if (!scope.$$phase) {
           scope.$apply();
         }
-      });
+      }
+      //Prefix all map events with 'map-', so eg 'click'
+      //for the map doesn't interfere with a normal 'click' event
+      mapObject.addEventListener(eventName, eventListeners[eventName]);
     });
   }
 
-  app.value('uiMapConfig', {})
+  /**
+   * remove map object added event listeners
+   *
+   * @param scope
+   * @param eventListeners
+   * @param mapObject
+   */
+  function removeMapEvents(scope, eventListeners, mapObject) {
+    angular.forEach(eventListeners, function (listener, eventName) {
+      mapObject.removeEventListener(eventName, listener);
+    });
+  }
+
+  app.value('uiMapConfig', {
+    initDelay: 200
+  })
     .directive('uiMap', ['uiMapConfig', '$window', '$parse', '$timeout',
       function (uiMapConfig, $window, $parse, $timeout) {
         var mapEvents = 'click dblclick rightclick rightdblclick maptypechange mousemove mouseover mouseout '
@@ -28,11 +81,24 @@
           + 'hotspotover hotspotout tilesloaded touchstart touchmove touchend longpress';
         var options = uiMapConfig || {};
         return {
-          restrict: 'A',
+          restrict: 'EA',
+          transclude: true,
+          template: '<div class="map-canvas"></div><div ng-transclude></div>',
           link: function (scope, elm, attrs) {
-            var map;
 
+            var map,
+                // map's event listeners
+                mapEventListeners = {};
+
+            // options
             var opts = angular.extend({}, options, scope.$eval(attrs.uiOptions));
+            // map cache
+            var uiMapCacheElm = attrs.uiMapCache,
+                uiMapCache    = uiMapCacheElm + "Map";
+
+            var mapElem = elm.find(".map-canvas");
+            mapElem.css("width", "100%");
+            mapElem.css("height", "100%");
 
             scope.$on("map.loaded", function (e, type) {
               if (type == "baidu" && !map) {
@@ -43,25 +109,27 @@
             if ($window.BMap && $window.BMap.Map) {
               $timeout(function () {
                 initMap();
-              }, 200);
+              }, opts.initDelay);
             }
 
             function initMap() {
-              if (opts.uiMapCache && $window[attrs.uiMapCache]) {
-                elm.replaceWith($window[attrs.uiMapCache]);
-                map = $window[attrs.uiMapCache + "Map"];
+              if (uiMapCacheElm && $window[uiMapCacheElm]) {
+                // get ui-map cache object
+                mapElem.replaceWith($window[uiMapCacheElm]);
+                map = $window[uiMapCache];
               } else {
-                map = new BMap.Map(elm[0], opts);
+                map = new BMap.Map(mapElem[0], opts);
+                // init map's center
                 if (opts.ngCenter &&
-                    angular.isNumber(opts.ngCenter.lat) &&
-                    angular.isNumber(opts.ngCenter.lng) &&
-                    angular.isNumber(opts.ngZoom)) {
+                  angular.isNumber(opts.ngCenter.lat) &&
+                  angular.isNumber(opts.ngCenter.lng) &&
+                  angular.isNumber(opts.ngZoom)) {
                   map.centerAndZoom(new BMap.Point(opts.ngCenter.lng, opts.ngCenter.lat), opts.ngZoom);
-                }else if (opts.ngCenter &&
-                    angular.isNumber(opts.ngCenter.lat) &&
-                    angular.isNumber(opts.ngCenter.lng)) {
+                } else if (opts.ngCenter &&
+                  angular.isNumber(opts.ngCenter.lat) &&
+                  angular.isNumber(opts.ngCenter.lng)) {
                   map.setCenter(new BMap.Point(opts.ngCenter.lng, opts.ngCenter.lat));
-                }else if(angular.isNumber(opts.ngZoom)) {
+                } else if (angular.isNumber(opts.ngZoom)) {
                   map.setZoom(opts.ngZoom);
                 }
 
@@ -82,7 +150,19 @@
               var model = $parse(attrs.uiMap);
               //Set scope variable for the map
               model.assign(scope, map);
-              bindMapEvents(scope, mapEvents, map, elm);
+              bindMapEvents(scope, mapEvents, map, elm, mapEventListeners);
+
+              // set ui-map cache object
+              if (uiMapCacheElm) {
+                // remove map event listeners and cache map element on controller destroy.
+                scope.$on("$destroy", function () {
+                  removeMapEvents(scope, mapEventListeners, map);
+                  if (!$window[uiMapCacheElm]) {
+                    $window[uiMapCache] = map;
+                    $window[uiMapCacheElm] = mapElem[0];
+                  }
+                });
+              }
             }
           }
         };
@@ -96,6 +176,7 @@
         return {
           link: function (scope, elm, attrs) {
             var opts = angular.extend({}, options, scope.$eval(attrs.uiOptions));
+            var eventListeners = {};
             var model = $parse(attrs.uiMapInfoWindow);
             var infoWindow = model(scope);
 
@@ -114,7 +195,7 @@
                 infoWindow = new $window.BMap.InfoWindow(elm[0], opts);
                 model.assign(scope, infoWindow);
               }
-              bindMapEvents(scope, infoWindowEvents, infoWindow, elm);
+              bindMapEvents(scope, infoWindowEvents, infoWindow, elm, eventListeners);
               /* The info window's contents dont' need to be on the dom anymore,
                google maps has them stored.  So we just replace the infowindow element
                with an empty div. (we don't just straight remove it from the dom because
@@ -143,8 +224,9 @@
         restrict: 'A',
         link: function (scope, elm, attrs) {
           scope.$watch(attrs[directiveName], function (newObject) {
+            var eventListeners = {};
             if (newObject) {
-              bindMapEvents(scope, events, newObject, elm);
+              bindMapEvents(scope, events, newObject, elm, eventListeners);
             }
           });
         }
--- javascripts/app.js
+++ javascripts/app.js
@@ -1,13 +1,40 @@
 'use strict';
 
-angular.module('uiMapBaiduApp', ["ui.map", "waypoint"])
-    .config(['uiMapLoadParamsProvider', function (uiMapLoadParamsProvider) {
-        uiMapLoadParamsProvider.setParams({
-            v: '1.5',
-            ak:'kp3ODQt4pkpHMW2Yskl2Lwee'// your map's develop key
-        });
-    }])
+angular.module('uiMapBaiduApp', ['ui.map', 'waypoint', 'ui.router'])
+    .config(['uiMapLoadParamsProvider', '$stateProvider', '$urlRouterProvider', '$locationProvider',
+        function (uiMapLoadParamsProvider, $stateProvider, $urlRouterProvider, $locationProvider) {
+
+            //$locationProvider.html5Mode(true);
+
+            uiMapLoadParamsProvider.setParams({
+                v: '2.0',
+                ak: 'kp3ODQt4pkpHMW2Yskl2Lwee'// your map's develop key
+            });
+
+            $urlRouterProvider.otherwise("/cache");
+
+            $stateProvider
+                .state('cache', {
+                    url: "/cache",
+                    templateUrl: "partials/cache.html",
+                    controller: "MapCacheCtrl"
+                })
+                .state('nocache', {
+                    url: "/nocache",
+                    templateUrl: "partials/nocache.html",
+                    controller: "MapNoCacheCtrl"
+                })
+        }])
     .controller('MapCtrl', ['$scope', function ($scope) {
+        //$scope.mapOptions = {
+        //    ngCenter: {
+        //        lat: 33,
+        //        lng: 111
+        //    },
+        //    ngZoom: 5
+        //};
+    }])
+    .controller('MapCacheCtrl', ['$scope', function ($scope) {
         $scope.myMarkers = [];
 
         $scope.mapOptions = {
@@ -23,13 +50,66 @@ angular.module('uiMapBaiduApp', ["ui.map", "waypoint"])
             overview: true,
             locatecity: true,
             // map-self config
-            resizeEnable: true,
-            // ui map config
-            uiMapCache: false
+            resizeEnable: true
         };
 
-        $scope.$watch('myMap', function(map) {
-            map&&map.setCurrentCity("上海");
+        $scope.$watch('myMap', function (map) {
+            if (map) {
+                map.setCurrentCity("上海");
+                map.enableDragging();
+            }
+        });
+
+        $scope.addMarker = function ($event, $params) {
+            var marker = new BMap.Marker(
+                $params[0].point
+            );
+
+            $scope.myMarkers.push(marker);
+            $scope.myMap.addOverlay(marker)
+        };
+
+        $scope.setZoomMessage = function (zoom) {
+            $scope.zoomMessage = 'You just zoomed to ' + zoom + '!';
+            console.log(zoom, 'zoomed');
+        };
+
+        $scope.openMarkerInfo = function (e, marker) {
+            e.preventDefault();
+            e.stopPropagation();
+            $scope.currentMarker = marker;
+            $scope.currentMarkerLat = marker.getPosition().lat;
+            $scope.currentMarkerLng = marker.getPosition().lng;
+
+            $scope.myMap.addOverlay($scope.myInfoWindow);
+            $scope.myMap.openInfoWindow($scope.myInfoWindow, marker.getPosition());
+        };
+
+        $scope.setMarkerPosition = function (marker, lat, lng) {
+            marker.setPosition(new BMap.Point(lng, lat));
+        };
+    }])
+    .controller('MapNoCacheCtrl', ['$scope', function ($scope) {
+        $scope.myMarkers = [];
+
+        $scope.mapOptions = {
+            ngCenter: {
+                lat: 33,
+                lng: 111
+            },
+            ngZoom: 5,
+            // map plugin config
+            toolbar: true,
+            scrollzoom: true,
+            maptype: true,
+            overview: true,
+            locatecity: true,
+            // map-self config
+            resizeEnable: true
+        };
+
+        $scope.$watch('myMap', function (map) {
+            map && map.setCurrentCity("上海");
         });
 
         $scope.addMarker = function ($event, $params) {
